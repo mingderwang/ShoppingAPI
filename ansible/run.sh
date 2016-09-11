1ca45c19e012cfe14dea5838856e224f9b70c5c6
tyoras <tyoras@gmail.com>
2016-09-11 19:35:38 +0200

    WIP

--- ansible/run.sh
+++ ansible/run.sh
@@ -1,18 +1,3 @@
 #!/bin/sh
-# Deploy in local containers:
-# ./run.sh
-#
-# Deploy a specific playbook on a specific env:
-# INVENTORY=env/inventory PLAYBOOK=playbook.yml ./run.sh
-set -eux
 
-#if [ -z "${VIRTUAL_ENV-}" ]; then
-#    Please activate a virtualenv
-#    exit 1
-#fi
-
-ansible-galaxy install --role-file requirements.yml --roles-path roles
-
-#[ $USER = jenkins ] && user="--user jenkins-back" || user=""
-
-ansible-playbook -v --inventory ${INVENTORY-local/inventory} ${PLAYBOOK-local.yml} # $user
+ansible-playbook deploy.yml -v --check
